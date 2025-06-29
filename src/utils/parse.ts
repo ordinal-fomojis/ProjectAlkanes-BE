75ebee0b9ea3c9de22f8fb2fca98b642d0019af2
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-06-29 11:22:36 +1000

    tests for parse

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,24 +1,6 @@
 import z from "zod"
 import { fromZodError } from "zod-validation-error"
 
-export class ValidationError extends Error {
-  constructor(result: z.SafeParseError<unknown>) {
-    let error = result.error
-    // If the schema was a union, it is likely that the object was close to one of the union types, 
-    // and the other is wildly different. In this case, the error message will be ridiculously long, because one of the
-    // schemas will be wrong all over the place. To simplify, we will only select the simplest error out of the unions.
-    const issue = result.error.issues[0]
-    if (issue != null && result.error.issues.length === 1 && issue.code === 'invalid_union') {
-      const simplestIssue = issue.unionErrors.reduce((a: z.ZodError | null, b) => 
-        a == null ? b : (a.issues.length < b.issues.length ? a : b), null)
-      if (simplestIssue != null) {
-        error = simplestIssue
-      }
-    }
-    super(fromZodError(error).message)
-  }
-}
-
 export function parse<T extends z.ZodTypeAny>(schema: T, obj: unknown): z.infer<T> {
   const result = schema.safeParse(obj)
   if (!result.success) {
@@ -27,12 +9,9 @@ export function parse<T extends z.ZodTypeAny>(schema: T, obj: unknown): z.infer<
     // schemas will be wrong all over the place. To simplify, we will only select the simplest error out of the unions.
     let error = result.error
     const issue = result.error.issues[0]
-    if (issue != null && result.error.issues.length === 1 && issue.code === 'invalid_union') {
-      const simplestIssue = issue.unionErrors.reduce((a: z.ZodError | null, b) => 
-        a == null ? b : (a.issues.length < b.issues.length ? a : b), null)
-      if (simplestIssue != null) {
-        error = simplestIssue
-      }
+    if (issue != null && result.error.issues.length === 1 && issue.code === 'invalid_union' && issue.unionErrors.length > 0) {
+      const simplestIssue = issue.unionErrors.reduce((a, b) => a.issues.length < b.issues.length ? a : b)
+      error = simplestIssue
     }
 
     throw new Error(fromZodError(error).message)
