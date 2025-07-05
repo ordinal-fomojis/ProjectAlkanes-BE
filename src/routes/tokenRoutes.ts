279f82164f23e02ef91dbb5c16d4b0d6559e78bb
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-07-05 17:50:18 +1000

    rename limit to page size

--- src/routes/tokenRoutes.ts
+++ src/routes/tokenRoutes.ts
@@ -9,7 +9,7 @@ const router = Router();
 const ParamsSchema = z.object({
   search: z.string(),
   page: z.number({ coerce: true }).optional(),
-  limit: z.number({ coerce: true }).optional(),
+  pageSize: z.number({ coerce: true }).optional(),
   orderBy: z.enum(['pendingMints', 'name', 'symbol', 'deployTimestamp', 'percentageMinted']).optional(),
   order: z.enum(['asc', 'desc']).optional()
 })
@@ -19,12 +19,12 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
   const {
     search,
     page = 1,
-    limit = 10,
+    pageSize = 20,
     orderBy = 'deployTimestamp',
     order = 'asc'
   } = parse(ParamsSchema, req.query)
   const service = new AlkaneTokenService()
-  const tokens = await service.searchAlkaneTokens(search, page, limit, { field: orderBy, order });
+  const tokens = await service.searchAlkaneTokens(search, page, pageSize, { field: orderBy, order });
   
   const tokenDetails = tokens.map(token => {
     const preminedSupply = toAlkane(token.preminedSupply)
