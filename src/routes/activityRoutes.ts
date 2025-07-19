8f02f9be0d48e9b721b20b88d44d9250b5e76dad
ElvisCo02 <elvisco2002@gmail.com>
2025-07-19 21:14:19 +0200

    build fix

--- src/routes/activityRoutes.ts
+++ src/routes/activityRoutes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Response, Router } from 'express'
 import { authenticateJWT, AuthenticatedRequest } from '../middleware/auth.js'
 import { MintTransactionService } from '../services/MintTransactionService.js'
 import { AlkaneTokenService } from '../services/AlkaneTokenService.js'
