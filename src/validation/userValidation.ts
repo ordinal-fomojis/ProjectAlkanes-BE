5ea86e6703d558ef4a8094a69c3066dc6b1bfbc6
ElvisCo02 <elvisco2002@gmail.com>
2025-07-22 14:49:36 +0200

    Taproot/Ordinal addresses support added

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -5,7 +5,7 @@ export const createUserSchema = Joi.object({
     .required()
     .min(26) // Minimum Bitcoin address length
     .max(90) // Maximum Bitcoin address length
-    .pattern(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$|^bc1[a-z0-9]{39,59}$|^[2-9A-HJ-NP-Za-km-z]{26,35}$/)
+    .pattern(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$|^bc1[a-z0-9]{39,59}$|^bc1p[a-z0-9]{39,59}$|^[2-9A-HJ-NP-Za-km-z]{26,35}$/)
     .messages({
       'string.empty': 'Wallet address cannot be empty',
       'string.min': 'Wallet address must be at least 26 characters long',
@@ -20,7 +20,7 @@ export const walletAddressSchema = Joi.object({
     .required()
     .min(26)
     .max(90)
-    .pattern(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$|^bc1[a-z0-9]{39,59}$|^[2-9A-HJ-NP-Za-km-z]{26,35}$/)
+    .pattern(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$|^bc1[a-z0-9]{39,59}$|^bc1p[a-z0-9]{39,59}$|^[2-9A-HJ-NP-Za-km-z]{26,35}$/)
     .messages({
       'string.empty': 'Wallet address cannot be empty',
       'string.min': 'Wallet address must be at least 26 characters long',
