0599c56aea3789ba0e8323e8eb81d6a0c4fd7724
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-08-31 15:42:36 +1000

    fix bigdecimal export

--- src/utils/big-decimal.ts
+++ src/utils/big-decimal.ts
@@ -1,4 +1,4 @@
 import bd from 'js-big-decimal'
 
 // Workaround because bigDecimal is CJS, so doesn't work well with out ESM project
-export const bigDecimal = bd.default
+export const bigDecimal = typeof bd === 'function' ? bd : bd.default
