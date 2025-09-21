4b412968a3c9d7739b818f732c74f82a1f9eac9b
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-09-21 15:44:25 +1000

    init function creates .env file

--- plopfile.ts
+++ plopfile.ts
@@ -1,7 +1,7 @@
 import { InteractiveBrowserCredential } from "@azure/identity"
 import { SecretClient } from "@azure/keyvault-secrets"
 import { config, set } from '@dotenvx/dotenvx'
-import { existsSync, readdirSync, readFileSync } from 'fs'
+import { existsSync, readdirSync } from 'fs'
 import { readFile, writeFile } from "fs/promises"
 import { Answers } from 'inquirer'
 import { NodePlopAPI } from 'plop'
@@ -22,35 +22,55 @@ const environments = readdirSync('env').map(file => file.replace('.env.', '')).f
 export default function (plop: NodePlopAPI) {
   plop.setGenerator('init', {
     description: 'Initialize dotenvx in your project',
-    prompts: [],
+    prompts: [
+      {
+        type: 'confirm',
+        name: 'overwrite',
+        message: 'A .env file already exists. Do you want to overwrite it?',
+        when: () => existsSync('.env')
+      }
+    ],
     actions: [
-      async function init() {
+      async function init(answers) {
         const vaultName = "shovel-kv"
         const secretName = "DotenvPrivateKeyNonProd"
-        const fileName = 'env/.env.keys'
+        const keyFilePath = 'env/.env.keys'
+        const defaultEnv = 'dev'
         
         const credential = new InteractiveBrowserCredential({ additionallyAllowedTenants: ['*'] })
         const client = new SecretClient(`https://${vaultName}.vault.azure.net`, credential)
         const privateKey = (await client.getSecret(secretName)).value
 
         const nonprodEnvs = environments.filter(env => env !== 'prod')
-        if (existsSync(fileName)) {
-          let content = await readFile(fileName, 'utf-8')
+        let message = ''
+        if (existsSync(keyFilePath)) {
+          let content = await readFile(keyFilePath, 'utf-8')
           for (const env of nonprodEnvs) {
             const name = `DOTENV_PRIVATE_KEY_${env.toUpperCase()}`
             content = content.replace(new RegExp(`^${name}=.*$`, 'm'), `${name}="${privateKey}"`)
           }
-          await writeFile(fileName, content)
-          return `Updated private keys in ${fileName}`
+          await writeFile(keyFilePath, content)
+          message = `Updated private keys in ${keyFilePath}`
         } else {
           const keyValues = nonprodEnvs.map(env => `# .env.${env}\nDOTENV_PRIVATE_KEY_${env.toUpperCase()}="${privateKey}"`).join('\n\n')
           const fileContent = `${PRIVATE_KEY_FILE_HEADER}\n\n${keyValues}`
-          await writeFile(fileName, fileContent)
-          return `Created ${fileName} with private keys`
+          await writeFile(keyFilePath, fileContent)
+          message = `Created ${keyFilePath} with private keys`
         }
+
+        const devFileContents = await decryptEnvFile(`env/.env.${defaultEnv}`)
+        if (answers.overwrite === true || !existsSync('.env')) {
+          await writeFile('.env', devFileContents)
+          message += ` and created .env from env/.env.${defaultEnv}`
+        } else {
+          message += ` and did not edit existing .env file`
+        }
+
+        return message
       }
     ]
   })
+
   plop.setGenerator('set-env-variable', {
     description: 'Set a new, or update an existing environment variable',
     prompts: [
@@ -105,21 +125,25 @@ export default function (plop: NodePlopAPI) {
       }
     ],
     actions: [
-      function decryptEnv(answers) {
+      async function decryptEnv(answers) {
         const envFileName = `env/.env.${answers.environment}`
-        const { parsed } = config({ path: envFileName, quiet: true })
-
-        if (parsed == null) {
-          throw new Error(`Failed to parse .env.${answers.environment}`)
-        }
-
-        let file = readFileSync(envFileName, 'utf-8')
-        for (const [key, value] of Object.entries(parsed)) {
-          file = file.replace(new RegExp(`^${key}=.*$`, 'm'), `${key}="${value}"`)
-        }
-
-        return `Decrypted ${envFileName}\n\n${file}`
+        return `Decrypted ${envFileName}\n\n${await decryptEnvFile(envFileName)}`
       }
     ]
   })
+}
+
+async function decryptEnvFile(path: string) {
+  const { parsed } = config({ path, quiet: true })
+
+  if (parsed == null) {
+    throw new Error(`Failed to parse ${path}`)
+  }
+
+  let fileContent = await readFile(path, 'utf-8')
+  for (const [key, value] of Object.entries(parsed)) {
+    fileContent = fileContent.replace(new RegExp(`^${key}=.*$`, 'm'), `${key}="${value}"`)
+  }
+
+  return fileContent
 }
