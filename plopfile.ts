72117bbbca5fc7c640668b6fea7f314fbef19de9
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-10-06 17:49:49 +1100

    add comment about weird path

--- plopfile.ts
+++ plopfile.ts
@@ -92,6 +92,10 @@ export default function (plop: NodePlopAPI) {
         const { name } = schema.parse(answers)
         const formattedName = name.toUpperCase().replace(/[^A-Z0-9_]/g, '_')
         for (const env of environments) {
+          // WORKAROUND: You do not need the private key to encrypt and env var (it uses public key)
+          // However, if you do have a private key set, and it is wrong, it validates the key and fails the command
+          // We may have a dummy private key set, as it's part of the template, so to prevent dotenvx trying to
+          // validate it, we set the envKeysFile to an invalid file path, so it doesn't try to validate the key
           set(formattedName, answers[`${env}_value`] as string, { path: `env/.env.${env}`, encrypt: true, envKeysFile: '.x' })
         }
         return 'Set environment variable'
